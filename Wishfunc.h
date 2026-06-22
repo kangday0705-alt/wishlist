826adc3e4bbfe7ba31cac1163c905076b71c7264
jiwon <kangday0705@gmail.com>
2026-06-23 02:49:30 +0900

    Initial commit add Wish, Checklist, Date classes

---
+++ Wishfunc.h
@@ -0,0 +1,75 @@
+#pragma once
+#include <ctime>
+#include <vector>
+using namespace std;
+
+class Date {
+public:
+  int year;
+  int month;
+  int day;
+};
+
+class Checklist {
+private:
+
+  string check;
+  bool isCheckAchieved;  // 체크리스트 달성
+
+public:
+
+  bool getisCheckAchieved() {
+    return isCheckAchieved;
+  }
+
+  void setcheck(string checkk) {
+    check = checkk;
+  }
+
+  void checkCheck() {
+    isCheckAchieved = true;
+  }
+
+  void uncheckCheck() {
+    isCheckAchieved = false;
+  }
+
+};
+
+class Wish {
+private:
+  string name;          // 위시 이름
+  int Balance;          //저축조건액
+  int currBalance;
+
+  vector<Checklist> checklist;
+
+  Date date;           // 목표날짜
+
+  int requiredCount;
+  bool isUnlocked;           // 해금 여부
+  bool isCompleted;          // 완료 여부
+
+public:
+  Wish(string namee = " ", int count = 0) {
+    
+    name = namee;
+    requiredCount = count;
+    Balance = 0;
+    currBalance = 0;
+    isUnlocked = false;
+    isCompleted = false;
+  }
+
+  void setrequiredCount(int);
+  void setBalance(int);
+  void setcurrBalance(int);
+  void setDate(Date);
+  void addchecklist(string);
+  void completecheck(int idx);
+  bool getIsUnlocked() {
+    return isUnlocked; 
+  }
+  void WishUnlock();
+};
+
